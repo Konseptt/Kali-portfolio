c483e970fdd3c2b648df0e93eac6cf643e53e082
konsept <sharmaranjan816@gmail.com>
2025-02-16 16:04:41 +0530

    Deploy to GitHub Pages with docs folder

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
   basePath: '/Kali-portfolio',
+  assetPrefix: '/Kali-portfolio/',
 }
 
 module.exports = nextConfig
