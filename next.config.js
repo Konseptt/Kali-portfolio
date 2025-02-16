8585eb7438c192bc4c4067f488f0f91cce3c1025
konsept <sharmaranjan816@gmail.com>
2025-02-16 16:15:01 +0530

    Add image loader configuration

--- next.config.js
+++ next.config.js
@@ -3,11 +3,9 @@ const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
-    loader: 'custom',
-    loaderFile: './utils/imageLoader.ts',
   },
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
 }
 
 module.exports = nextConfig
